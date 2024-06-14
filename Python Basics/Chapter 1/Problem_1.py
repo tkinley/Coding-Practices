09ea55002f21f7935b2f533eb3501cb1b47fbc0e
Kinley Tenzin <35526271+tkinley@users.noreply.github.com>
2024-06-14 11:52:08 -0600

    Update and rename Problem_1.py to Problem_1

--- Python Basics/Chapter 1/Problem_1.py
+++
@@ -1,4 +0,0 @@
-# Challenge: Many people keep time using a 24 hour clock (11 is 11am and 23 is 11pm, 0 is midnight). If it is currently 13 and you set your alarm to go off in 50 hours, it will be 15 (3pm). Write a Python program to solve the general version of the above problem.
-# Ask the user for the time now (in hours), and then ask for the number of hours to wait for the alarm. Your program should output what the time will be on the clock when the alarm goes off.
-
-
